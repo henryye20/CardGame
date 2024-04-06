0d639b5cc63dd6bb0f43b3ff5ae2ccf747d19187
henryye20 <henryye2006@gmail.com>
2024-04-06 13:26:12 -0700

    Implemented Enemy AI

---
+++ enemies/someguy/someguy_attack_action.gd
@@ -0,0 +1,38 @@
+extends EnemyAction
+
+@export var damage:= 7
+
+
+func perform_action() -> void:
+	#checks for valid target
+	if not enemy or not target:
+		return
+	#enemy animation
+	var tween:= create_tween().set_trans(Tween.TRANS_QUINT)
+	var start:= enemy.global_position
+	#*32 is + 32 for vectors for some reason
+	var end := target.global_position + Vector2.RIGHT * 32
+	var damage_effect:= DamageEffect.new()
+	var target_array: Array[Node] = [target]
+	
+	damage_effect.amount = damage
+	
+	
+	tween.tween_property(enemy, "global_position", end, 1.0)
+	tween.tween_property(enemy.sprite_2d,"rotation_degrees",360,.2)
+	tween.tween_property(enemy.sprite_2d,"rotation_degrees",0,.3)
+	tween.parallel().tween_property(enemy, "scale",Vector2(1.5,1.5),0.1)
+	tween.tween_property(enemy, "scale",Vector2(1,1),0.1)
+	tween.tween_callback(damage_effect.execute.bind(target_array))
+	
+	tween.tween_interval(0.25)
+	
+	print("tweening")
+	tween.tween_property(enemy,"global_position", start, 0.4)
+	
+	tween.finished.connect(
+		func():
+			Events.enemy_action_completed.emit(enemy)
+	)
+	
+	
---
+++ enemies/someguy/someguy_summon_action.gd
@@ -0,0 +1,19 @@
+extends EnemyAction
+#not tutorial code :D
+@export var hp_threshold := 5
+
+var already_used := false
+
+
+func is_performable() -> bool:
+	if not enemy or already_used:
+		return false
+	
+	var is_low := enemy.stats.health <- hp_threshold
+	already_used = is_low
+	
+	return is_low
+
+func perform_action() -> void:
+	print("Summon")
+	Events.enemy_action_completed.emit(enemy)
